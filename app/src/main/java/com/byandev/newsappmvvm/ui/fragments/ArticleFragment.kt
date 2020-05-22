f4969c231f1063f532490fd63a4f78ea9d95fa71
byansanur <ratbyansa25@gmail.com>
2020-05-22 21:35:12 +0700

    saved news and delete on room databases

--- app/src/main/java/com/byandev/newsappmvvm/ui/fragments/ArticleFragment.kt
+++ app/src/main/java/com/byandev/newsappmvvm/ui/fragments/ArticleFragment.kt
@@ -8,6 +8,7 @@ import androidx.navigation.fragment.navArgs
 import com.byandev.newsappmvvm.R
 import com.byandev.newsappmvvm.ui.NewsActivity
 import com.byandev.newsappmvvm.ui.viewModel.News.NewsViewModel
+import com.google.android.material.snackbar.Snackbar
 import kotlinx.android.synthetic.main.fragment_article.*
 
 class ArticleFragment : Fragment(R.layout.fragment_article) {
@@ -25,6 +26,11 @@ class ArticleFragment : Fragment(R.layout.fragment_article) {
         webView.apply {
             webViewClient = WebViewClient()
             loadUrl(article.url)
+        }
+
+        fab.setOnClickListener {
+            viewModel.saveArticle(article)
+            Snackbar.make(view, "Article saved successfully", Snackbar.LENGTH_LONG).show()
         }
     }
 }
--- app/src/main/java/com/byandev/newsappmvvm/ui/fragments/SavedNewsFragment.kt
+++ app/src/main/java/com/byandev/newsappmvvm/ui/fragments/SavedNewsFragment.kt
@@ -3,14 +3,17 @@ package com.byandev.newsappmvvm.ui.fragments
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
 import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.ItemTouchHelper
 import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.byandev.newsappmvvm.R
 import com.byandev.newsappmvvm.ui.NewsActivity
 import com.byandev.newsappmvvm.ui.adapters.NewsAdapter
 import com.byandev.newsappmvvm.ui.viewModel.News.NewsViewModel
+import com.google.android.material.snackbar.Snackbar
 import kotlinx.android.synthetic.main.fragment_saved_news.*
-import kotlinx.android.synthetic.main.fragment_search_news.*
 
 class SavedNewsFragment : Fragment(R.layout.fragment_saved_news) {
 
@@ -33,6 +36,41 @@ class SavedNewsFragment : Fragment(R.layout.fragment_saved_news) {
                 bundle
             )
         }
+
+        val itemTouchHelperCallback = object : ItemTouchHelper.SimpleCallback(
+            ItemTouchHelper.UP or ItemTouchHelper.DOWN,
+            ItemTouchHelper.LEFT or ItemTouchHelper.RIGHT
+        ) {
+            override fun onMove(
+                recyclerView: RecyclerView,
+                viewHolder: RecyclerView.ViewHolder,
+                target: RecyclerView.ViewHolder
+            ): Boolean {
+                return true
+            }
+
+            override fun onSwiped(viewHolder: RecyclerView.ViewHolder, direction: Int) {
+                val position = viewHolder.adapterPosition
+                val article = newsAdapter.differ.currentList[position]
+                viewModel.deleteArticle(article)
+                Snackbar.make(view, "Article deleted successfully", Snackbar.LENGTH_LONG).apply {
+                    setAction("Undo") {
+                        viewModel.saveArticle(article)
+                    }
+                    show()
+                }
+            }
+
+        }
+
+        ItemTouchHelper(itemTouchHelperCallback).apply {
+            attachToRecyclerView(rvSavedNews)
+        }
+
+        viewModel.getSavedNews().observe(viewLifecycleOwner, Observer { article ->
+            newsAdapter.differ.submitList(article)
+
+        })
     }
 
     private fun setupRecyclerView() {
